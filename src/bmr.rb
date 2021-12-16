c847de2a0acb7c5e1cd948a3f9225c04a1da9291
jpmajarres <jmanjarres@gmail.com>
2021-12-17 00:55:43 +1000

    table format

--- src/bmr.rb
+++ src/bmr.rb
@@ -1,6 +1,10 @@
-weight = 63
+weight = 64
 height = 161
 age = 31
 
 bmr = ((10 * (weight ) + (6.25 * height) - ( 5* age) - 161) * 1.55).round
-puts bmr
+cal_d = (bmr - ((7700 * 6)/30))
+new_bmr = (bmr-cal_d)
+puts cal_d
+puts bmr 
+puts new_bmr
--- src/table.rb
+++ src/table.rb
@@ -3,7 +3,7 @@ require 'colorize'
 # require_relative 'methods.rb'
 # require_relative 'bmr.rb'
 
-bmr = 2046
+bmr = 2062
 e_weigh = 58
 bwhen = 30
 
@@ -16,12 +16,11 @@ rows << ['Macro Percentages', "30(c), 40(p), 30(f)"]
 
 table = Terminal::Table.new :title => "BRIEF SUMMARY", :headings => [], :rows => rows
 puts table
-puts ""
+puts "\n" 
 puts "(c) = Carbs"
 puts "(p) = Protein"
 puts "(f) = Fats"
-puts ""
+puts "\n" 
 puts "To maintain your current weight requires about (#{bmr}). Calories per day."
-puts "To reach your goal of #{e_weigh} kg in #{bwhen} days at your current body weight and activity level, requires about: "
+puts "To reach your goal of #{e_weigh} kg in #{bwhen} days at your current body weight and activity level, requires about: 1540 Calories per day "
 
-puts table
